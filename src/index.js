a3290961b0b90d2aca6fde8c137b6a1d8c11053b
VesperSpoony <714694113@qq.com>
2024-07-06 11:20:10 +0800

    day7

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import { Provider } from 'react-redux'
 import store from './store'
+import 'normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
 
